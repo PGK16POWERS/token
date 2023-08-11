1b6c6d4e69d9e6b701ac74111db962684d4da287
PGK16POWERS <136324051+PGK16POWERS@users.noreply.github.com>
2023-08-11 18:58:30 +0000

    solid push

--- jscriptfolders/indFunc.js
+++ jscriptfolders/indFunc.js
@@ -19,74 +19,159 @@ document.addEventListener("DOMContentLoaded", ()=> {
     form.appendChild(remove);
 
     const formHeader = document.createElement("h1");
-    formHeader.textContent = "NewsLetter";
+    formHeader.textContent = "";
     formHeader.setAttribute("class","formHeader");
     form.appendChild(formHeader);
+
+    const userSignedUp = localStorage.getItem("signedUp");
     
-    const formContParent = document.createElement("div");
-    formContParent.setAttribute("class","formContParent");
-    form.appendChild(formContParent);
+    function signEmUp() {
 
-    const formChildName = document.createElement("div");
-    formChildName.setAttribute("class", "formChild");
-    formContParent.appendChild(formChildName);
+        formHeader.textContent = "NewsLetter";
 
-    const nameLabel = document.createElement("label");
-    nameLabel.textContent = "Name";
-    nameLabel.setAttribute("class","form-label");
-    formChildName.appendChild(nameLabel);
+        const formContParent = document.createElement("div");
+        formContParent.setAttribute("class","formContParent");
+        form.appendChild(formContParent);
+        
+        const formChildName = document.createElement("div");
+        formChildName.setAttribute("class", "formChild");
+        formContParent.appendChild(formChildName);
+        
+        const nameLabel = document.createElement("label");
+        nameLabel.textContent = "Name";
+        nameLabel.setAttribute("class","form-label");
+        formChildName.appendChild(nameLabel);
+        
+        const nameInp = document.createElement("input")
+        nameInp.setAttribute("type","text");
+        nameInp.setAttribute("id","name-inp");
+        nameInp.setAttribute("name","name")
+        formChildName.appendChild(nameInp);
+        
+        const errMessName = document.createElement("div");
+        errMessName.textContent = "";
+        errMessName.setAttribute("class","errmess");
+        formChildName.appendChild(errMessName);
+        
+        ///////////////////////////////////////////////////////
+        const formChildNumber = document.createElement("div");
+        formChildNumber.setAttribute("class", "formChild");
+        formContParent.appendChild(formChildNumber);
+        
+        const numberLabel = document.createElement("label");
+        numberLabel.textContent = "Number";
+        numberLabel.setAttribute("class","form-label");
+        formChildNumber.appendChild(numberLabel);
+        
+        const numberInp = document.createElement("input")
+        numberInp.setAttribute("type","tel");
+        numberInp.setAttribute("id","number-inp");
+        numberInp.setAttribute("name","number")
+        formChildNumber.appendChild(numberInp);
+        
+        const errMessNumber = document.createElement("div");
+        errMessNumber.textContent = "";
+        errMessNumber.setAttribute("class","errmess");
+        formChildNumber.appendChild(errMessNumber);
+        
+        /////////////////////////////////////////////////////
+        const formChildEmail = document.createElement("div");
+        formChildEmail.setAttribute("class", "formChild");
+        formContParent.appendChild(formChildEmail);
+        
+        const emailLabel = document.createElement("label");
+        emailLabel.textContent = "Email";
+        emailLabel.setAttribute("class","form-label");
+        formChildEmail.appendChild(emailLabel);
+        
+        const emailInp = document.createElement("input")
+        emailInp.setAttribute("type","email");
+        emailInp.setAttribute("id","number-inp");
+        emailInp.setAttribute("name","email")
+        formChildEmail.appendChild(emailInp);
+        
+        const errMessEmail = document.createElement("div");
+        errMessEmail.textContent = "";
+        errMessEmail.setAttribute("class","errmess");
+        formChildEmail.appendChild(errMessEmail);
+        
+        const subbtn = document.createElement("button");
+        subbtn.textContent = "Send";
+        subbtn.setAttribute("type","submit");
+        subbtn.setAttribute("id","subbtn");
+        subbtn.setAttribute("class","subbutton");
+        form.appendChild(subbtn);
+        
+        ////////////////////////////////////////////////////
 
-    const nameInp = document.createElement("input")
-    nameInp.setAttribute("type","text");
-    nameInp.setAttribute("id","name-inp");
-    nameInp.setAttribute("name","name")
-    formChildName.appendChild(nameInp);
-
-    //////////////////////////////////
-    const formChildNumber = document.createElement("div");
-    formChildNumber.setAttribute("class", "formChild");
-    formContParent.appendChild(formChildNumber);
-
-    const numberLabel = document.createElement("label");
-    numberLabel.textContent = "Number";
-    numberLabel.setAttribute("class","form-label");
-    formChildNumber.appendChild(numberLabel);
-
-    const numberInp = document.createElement("input")
-    numberInp.setAttribute("type","tel");
-    numberInp.setAttribute("id","number-inp");
-    numberInp.setAttribute("name","number")
-    formChildNumber.appendChild(numberInp);
-
-    const errMessage = document.createElement("div");
-    errMessage.textContent = ""
-    errMessage.setAttribute("class","errMes");
-    formChildNumber.appendChild(errMessage);
-
-    //////////////////////////////////
-    const formChildEmail = document.createElement("div");
-    formChildEmail.setAttribute("class", "formChild");
-    formContParent.appendChild(formChildEmail);
-
-    const emailLabel = document.createElement("label");
-    emailLabel.textContent = "Email";
-    emailLabel.setAttribute("class","form-label");
-    formChildEmail.appendChild(emailLabel);
-
-    const emailInp = document.createElement("input")
-    emailInp.setAttribute("type","email");
-    emailInp.setAttribute("id","number-inp");
-    emailInp.setAttribute("name","email")
-    formChildEmail.appendChild(emailInp);
-
-    const subbtn = document.createElement("button");
-    subbtn.textContent = "Send";
-    subbtn.setAttribute("type","submit");
-    subbtn.setAttribute("id","subbtn");
-    subbtn.setAttribute("class","subbutton");
-    form.appendChild(subbtn);
-
-
+        form.addEventListener("submit", (event) => {
+            /////////////////////--NAME-BELOW--/////////////////////////
+            if (nameInp.value.length === 0) {
+                event.preventDefault();
+                errMessName.textContent = "Please Fill Name";
+                nameInp.classList.add("redborder");
+            } else {
+                const cleanedName = nameInp.value.replace(/[!@#$%^&*{}<>()+=?]/g, ''); // Remove special characters
+                nameInp.value = cleanedName; // Update the input field with cleaned value
+        
+                if (cleanedName.length === 0) {
+                    event.preventDefault();
+                    errMessName.textContent = "Remove special Character(s)";
+                    nameInp.classList.add("redborder");
+                } else {
+                    errMessName.textContent = "";
+                    nameInp.classList.add("greenborder");
+                    var isVaar = true;
+                }
+            }
+        
+            /////////////////////--NUMBER-BELOW--//////////////////////////
+            if (numberInp.value.length === 0) {
+                event.preventDefault();
+                errMessNumber.textContent = "Please Fill Number";
+                numberInp.classList.add("redborder");
+            } else {
+                const cleanedNumber = numberInp.value.replace(/\s/g, ''); // Remove white spaces
+        
+                if(/[!@#$%^&*{}<>()+=?a-zA-Z]/.test(cleanedNumber)){
+                    event.preventDefault();
+                    errMessNumber.textContent = "Remove special Character(s)";
+                    numberInp.classList.add("redborder");
+                } else if(cleanedNumber.length >= 11 || cleanedNumber.length <= 9 ) {
+                    event.preventDefault();
+                    errMessNumber.textContent = "Make Sure Number is correct";
+                    numberInp.classList.add("redborder");
+                } else {
+                    errMessNumber.textContent = "";
+                    numberInp.classList.add("greenborder");
+                    isVaar = true;
+                }
+            }
+        
+            //////////////////////--EMAIL-BELOW--//////////////////////////
+            if(emailInp.value.length === 0) {
+                event.preventDefault();
+                errMessEmail.textContent = "Please Fill Email";
+                emailInp.classList.add("redborder");
+            } else {
+                errMessEmail.textContent = "";
+                emailInp.classList.add("greenborder");
+                isVaar = true;
+            }
+    
+            /////////////////////--SET-ITEM-IN-LOCAL-STORAGE--////////////
+            if(errMessName.textContent === "" && errMessNumber.textContent === "" && errMessEmail.textContent === "") {
+                localStorage.setItem("signedUp","true");
+                console.log("This User Signed UP");
+            }
+        });
+    }
+    
+    if(!userSignedUp){
+        signEmUp()
+    } else {
+        formHeader.textContent = "Already Signed Up"
+    }
 
     newsBtn.addEventListener("click", ()=> {
         frontCont.style.display = "none";
@@ -97,4 +182,5 @@ document.addEventListener("DOMContentLoaded", ()=> {
             signUpForm.style.display = "none";
         })
     })
+    
 })
--- node.js
+++ node.js
@@ -1,14 +1,43 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const ejs = require("ejs");
+const helmet = require("helmet");
+const bodyParser = require("body-parser");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
+
+const uri = "mongodb+srv://PGK16POWERS:p59sRcfcKGnUFhI0@nahstore.tpctfgo.mongodb.net/?retryWrites=true&w=majority" ;
+
+const client = new MongoClient(uri, {
+    serverApi: {
+      version: ServerApiVersion.v1,
+      strict: true,
+      deprecationErrors: true,
+    }
+});
+
+app.set("view engine","ejs");
+//app.use(helmet());
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
 app.use(express.static("cssfolders"));
 app.use(express.static("htmlfolders"));
 app.use(express.static("jscriptfolders"));
 app.use(express.static("photos"));
+app.use(express.static("views"));
+
+async function mongoConnect() {
+    try {
+        await client.connect()
+        console.log("Database connected successfully.")
+    } catch (error) {
+        console.log("Error experienced while trying to connect database", error)
+    }
+}
 
 app.get("/",(req,res) => {
-    res.status(200).sendFile(path.join(__dirname, "index.html"));
+    res.render("index.ejs");
 })
 
 app.get("/innerlayout", (req,res)=> {
@@ -31,6 +60,29 @@ app.get("/perfumepage", (req,res)=> {
     res.sendFile(path.join(__dirname,'perfumepage.html'));
 });
 
+app.post("/newslletter", async (req,res) => {
+    const { name, number, email } = req.body;
+    const newUser = { name, number, email };
+    const db = client.db("BigMarjella");
+    const collection = db.collection("NAHSTORE");
+
+    try {
+        //CHECK IF USER ALREADY EXISTS
+        const doesUserExist = await collection.findOne({ email:email });
+        if(!doesUserExist){
+            const addUser = await collection.insertOne(newUser);
+            console.log("New doc created:",addUser);
+            res.render("/index.ejs");
+        } else {
+            res.render("")
+        }
+        } catch (error) {
+            console.log("Error experienced while trying to create mew doc:",error)
+        }
+    
+})
+
+mongoConnect()
 app.listen(4500, () => {
     console.log("server connected on port 4500");
 });
