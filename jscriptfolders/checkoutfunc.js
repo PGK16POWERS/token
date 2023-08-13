2eefadfab5ee397c6c65100038f09d389005fc73
PGK16POWERS <136324051+PGK16POWERS@users.noreply.github.com>
2023-08-13 11:53:05 +0000

    standard

--- jscriptfolders/checkoutfunc.js
+++ jscriptfolders/checkoutfunc.js
@@ -16,15 +16,36 @@ document.addEventListener("DOMContentLoaded",() => {
         receiptItem.setAttribute("class","receiptItem");
         ul.appendChild(receiptItem);
 
+        const leftSide = document.createElement("div");
+        leftSide.setAttribute("class","leftside");
+        receiptItem.appendChild(leftSide);
+
         const prodName = document.createElement("p");
         prodName.setAttribute("class","prodName");
         prodName.textContent = keys;
-        receiptItem.appendChild(prodName);
+        leftSide.appendChild(prodName);
 
         const prodPrice = document.createElement("p");
-        prodPrice.textContent = `R ${values}`;
+        prodPrice.textContent = `R ${values}.00`;
         prodPrice.setAttribute("class","total");
-        receiptItem.appendChild(prodPrice);
+        leftSide.appendChild(prodPrice);
+
+        const rightSide = document.createElement("div");
+        rightSide.setAttribute("class","rightside");
+        receiptItem.appendChild(rightSide);
+
+        const deleteAll = document.createElement("span");
+        deleteAll.textContent = "delete";
+        deleteAll.setAttribute("class","material-symbols-outlined");
+        deleteAll.setAttribute("id","deleteAll");
+        rightSide.appendChild(deleteAll);
+
+            deleteAll.addEventListener("click", () => {
+                localStorage.removeItem(keys);
+            })
+            // CREATE A FUNCTION THAT, WHEN THE DELETEALL BUTTON IS CLICKED, 
+            // THE KEY IS REMOVED AND THE DIV IS REMOVED, ALSO THE ITEMS PRICE IS 
+            // IS SUBTRACTED FROM THE TOTAL PRICE. 
     }
 
     totalPrice.textContent = `R ${addedUp.toFixed(2)}`;
