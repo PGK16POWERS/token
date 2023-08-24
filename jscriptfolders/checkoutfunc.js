bc09fbc8b3954141f7b7c6f782ef3dd6525d38de
Liberia (Kaiberius) <136324051+PGK16POWERS@users.noreply.github.com>
2023-08-24 13:48:11 +0000

    checkout updates

--- jscriptfolders/checkoutfunc.js
+++ jscriptfolders/checkoutfunc.js
@@ -1,59 +1,98 @@
-document.addEventListener("DOMContentLoaded",() => {
+document.addEventListener("DOMContentLoaded", () => {
     const ul = document.querySelector("#checkout-list");
     const totalPrice = document.querySelector("#total-price");
 
-    function retriveveData(){
-        for(let i = 0; i < localStorage.length; i++) {
-        const prodID = localStorage.key(i);
-        const storageItems = JSON.parse(localStorage.getItem(localStorage.key(i)));
-        console.log(prodID)
-
-        receiptItem(storageItems,prodID);
-
-        totalPrice.textContent = `R ${parseFloat(storageItems.price)}.00`
-        }
+    function updateQty(qtyElement, newQuantity) {
+        qtyElement.textContent = newQuantity;
     }
-    
+
+    function updateTotalPrice() {
+        totalPrice.textContent = `R ${calculateTotalPrice().toFixed(2)}`;
+    }
+
+    function calculateTotalPrice() {
+        var total = 0;
+
+        document.querySelectorAll(".receiptItem").forEach(card => {
+            var price = parseInt(card.querySelector(".prodPrice").textContent);
+            var qty = parseInt(card.querySelector(".qty").textContent);
+            total += price * qty;
+        });
+
+        return total;
+    }
 
     function receiptItem(storageItems, prodID) {
         const receiptItem = document.createElement("div");
-        receiptItem.setAttribute("class","receiptItem");
+        receiptItem.setAttribute("class", "receiptItem");
         ul.appendChild(receiptItem);
 
         const leftSide = document.createElement("div");
-        leftSide.setAttribute("class","leftside");
+        leftSide.setAttribute("class", "leftside");
         receiptItem.appendChild(leftSide);
 
         const prodName = document.createElement("p");
-        prodName.setAttribute("class","prodName");
+        prodName.setAttribute("class", "prodName");
         prodName.textContent = storageItems.name;
         leftSide.appendChild(prodName);
 
+        // ...
+
+        var quantity = 1; // You might want to adjust this based on your data
+
+        // ...
+
+        plus.addEventListener("click", () => {
+            quantity++;
+            updateQty(qty, quantity);
+            updateTotalPrice();
+        });
+
+        minus.addEventListener("click", () => {
+            if (quantity >= 2) {
+                quantity--;
+            }
+            updateQty(qty, quantity);
+            updateTotalPrice();
+        });
+
+        // ...
+
         const prodPrice = document.createElement("p");
         prodPrice.textContent = `R ${storageItems.price}.00`;
-        prodPrice.setAttribute("class","total");
+        prodPrice.setAttribute("class", "prodPrice");
         leftSide.appendChild(prodPrice);
 
-        const rightSide = document.createElement("div");
-        rightSide.setAttribute("class","rightside");
-        receiptItem.appendChild(rightSide);
-
-        const quntInp = document.createElement("input");
-        quntInp.setAttribute("type","tel");
-        quntInp.setAttribute("id","quntInp");
-        rightSide.appendChild(quntInp);
+        // ...
 
         const deleteAll = document.createElement("span");
         deleteAll.textContent = "delete";
-        deleteAll.setAttribute("class","material-symbols-outlined");
-        deleteAll.setAttribute("id","deleteAll");
-        rightSide.appendChild(deleteAll);
+        deleteAll.setAttribute("class", "material-symbols-outlined");
+        deleteAll.setAttribute("id", "deleteAll");
+        receiptItem.appendChild(deleteAll);
 
-            deleteAll.addEventListener("click", () => {
-                localStorage.removeItem(prodID);
-            })
+        deleteAll.addEventListener("click", () => {
+            ul.removeChild(receiptItem);
+            localStorage.removeItem(prodID);
+            updateTotalPrice();
+        });
 
+        // ...
+
+        // Call these functions only once, after setting up everything
+        updateQty(qty, quantity);
+        updateTotalPrice();
     }
 
-    retriveveData()
-}) 
+    function retrieveData() {
+        for (let i = 0; i < localStorage.length; i++) {
+            const prodID = localStorage.key(i);
+            const storageItems = JSON.parse(localStorage.getItem(localStorage.key(i)));
+            console.log(prodID);
+
+            receiptItem(storageItems, prodID);
+        }
+    }
+
+    retrieveData();
+});
