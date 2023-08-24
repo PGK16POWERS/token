1c1d2d6488d14fbb64c8338fcaba9752a79bf2c2
Liberia (Kaiberius) <136324051+PGK16POWERS@users.noreply.github.com>
2023-08-24 14:16:39 +0000

    newer checkout

--- jscriptfolders/checkoutfunc.js
+++ jscriptfolders/checkoutfunc.js
@@ -2,20 +2,27 @@ document.addEventListener("DOMContentLoaded", () => {
     const ul = document.querySelector("#checkout-list");
     const totalPrice = document.querySelector("#total-price");
 
-    function updateQty(qtyElement, newQuantity) {
-        qtyElement.textContent = newQuantity;
+    function retrieveData() {
+        for (let i = 0; i < localStorage.length; i++) {
+            const prodID = localStorage.key(i);
+            const storageItems = JSON.parse(localStorage.getItem(localStorage.key(i)));
+            receiptItem(storageItems, prodID);
+        }
+
+        updateTotalPrice();
     }
 
     function updateTotalPrice() {
-        totalPrice.textContent = `R ${calculateTotalPrice().toFixed(2)}`;
+        const total = calculateTotalPrice();
+        totalPrice.textContent = `R ${total.toFixed(2)}`;
     }
 
     function calculateTotalPrice() {
-        var total = 0;
+        let total = 0;
 
         document.querySelectorAll(".receiptItem").forEach(card => {
-            var price = parseInt(card.querySelector(".prodPrice").textContent);
-            var qty = parseInt(card.querySelector(".qty").textContent);
+            const price = parseInt(card.querySelector(".total").getAttribute("data-price"));
+            const qty = parseInt(card.querySelector(".qty").textContent);
             total += price * qty;
         });
 
@@ -36,34 +43,44 @@ document.addEventListener("DOMContentLoaded", () => {
         prodName.textContent = storageItems.name;
         leftSide.appendChild(prodName);
 
-        // ...
+        const prodPrice = document.createElement("p");
+        prodPrice.textContent = `R ${storageItems.price}.00`;
+        prodPrice.setAttribute("class", "total");
+        prodPrice.setAttribute("data-price", storageItems.price);
+        leftSide.appendChild(prodPrice);
 
-        var quantity = 1; // You might want to adjust this based on your data
+        var quantity = 1;
+        const qtyDiv = document.createElement("div");
+        qtyDiv.setAttribute("class", "qty-div");
+        receiptItem.appendChild(qtyDiv);
 
-        // ...
+        const minus = document.createElement("button");
+        minus.textContent = "-";
+        minus.setAttribute("class", "min-plus");
+        qtyDiv.appendChild(minus);
+
+        const qty = document.createElement("span");
+        qty.setAttribute("class", "qty");
+        qtyDiv.appendChild(qty);
+
+        const plus = document.createElement("button");
+        plus.textContent = "+";
+        plus.setAttribute("class", "min-plus");
+        qtyDiv.appendChild(plus);
 
         plus.addEventListener("click", () => {
             quantity++;
-            updateQty(qty, quantity);
+            qty.textContent = quantity;
             updateTotalPrice();
         });
 
         minus.addEventListener("click", () => {
             if (quantity >= 2) {
                 quantity--;
+                qty.textContent = quantity;
+                updateTotalPrice();
             }
-            updateQty(qty, quantity);
-            updateTotalPrice();
         });
-
-        // ...
-
-        const prodPrice = document.createElement("p");
-        prodPrice.textContent = `R ${storageItems.price}.00`;
-        prodPrice.setAttribute("class", "prodPrice");
-        leftSide.appendChild(prodPrice);
-
-        // ...
 
         const deleteAll = document.createElement("span");
         deleteAll.textContent = "delete";
@@ -72,26 +89,12 @@ document.addEventListener("DOMContentLoaded", () => {
         receiptItem.appendChild(deleteAll);
 
         deleteAll.addEventListener("click", () => {
-            ul.removeChild(receiptItem);
             localStorage.removeItem(prodID);
+            ul.removeChild(receiptItem);
             updateTotalPrice();
         });
 
-        // ...
-
-        // Call these functions only once, after setting up everything
-        updateQty(qty, quantity);
-        updateTotalPrice();
-    }
-
-    function retrieveData() {
-        for (let i = 0; i < localStorage.length; i++) {
-            const prodID = localStorage.key(i);
-            const storageItems = JSON.parse(localStorage.getItem(localStorage.key(i)));
-            console.log(prodID);
-
-            receiptItem(storageItems, prodID);
-        }
+        qty.textContent = quantity;
     }
 
     retrieveData();
